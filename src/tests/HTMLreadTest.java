56e3a644a8e83a0235bb80fc1779035a935f919a
Geoff Falk <geoff@maskmedia.org>
2013-02-07 12:52:32 +0000

    Start JUnit implementation

---
+++ src/tests/HTMLreadTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HTMLreadTest {
+
+	@Test
+	public void testHTMLread() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadUntil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSkipSpace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadString() {
+		fail("Not yet implemented");
+	}
+
+}
